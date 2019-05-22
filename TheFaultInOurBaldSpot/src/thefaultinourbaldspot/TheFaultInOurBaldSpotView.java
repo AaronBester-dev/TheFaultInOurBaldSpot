9850e805b2e94113fdb10df3bed400b400b4f240
BillNyeTheCommunistGuy <50926823+BillNyeTheCommunistGuy@users.noreply.github.com>
2019-05-22 15:06:03 -0400

    tesinbg

--- TheFaultInOurBaldSpot/src/thefaultinourbaldspot/TheFaultInOurBaldSpotView.java
+++ TheFaultInOurBaldSpot/src/thefaultinourbaldspot/TheFaultInOurBaldSpotView.java
@@ -25,6 +25,7 @@ public class TheFaultInOurBaldSpotView extends FrameView {
         super(app);
 //Whats up boizzzzz!!!
 //Aaron is a baby boi
+//asgjhsadgasdgasdgj
         initComponents();
 
         // status bar initialization - message timeout, idle icon and busy animation, etc
