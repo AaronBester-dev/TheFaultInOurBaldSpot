6309e2b580a8c995efd7d4692979251e6923a7e2
BillNyeTheCommunistGuy <50926823+BillNyeTheCommunistGuy@users.noreply.github.com>
2019-05-24 15:21:10 -0400

    A lot

--- TheFaultInOurBaldSpot/src/AlexForm.java
+++ TheFaultInOurBaldSpot/src/AlexForm.java
@@ -3,7 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.awt.Component;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 /**
  *
  * @author alexander.rejep819
@@ -13,10 +23,81 @@ public class AlexForm extends javax.swing.JFrame {
     /**
      * Creates new form AlexForm
      */
-    public AlexForm() {
-        initComponents();
+        private boolean checkCollision(javax.swing.JLabel _lbl, int _x, int _y) {
+//creating a temporary rectangle with (x, y) coordinates equal to where image is trying to move
+//also same width and height as original
+        Rectangle rect = new Rectangle(_lbl.getBounds().x + _x, _lbl.getBounds().y + _y, _lbl.getWidth(), _lbl.getHeight());
+
+//check if temporary rectangle intersect with wallLabel        
+        if (rect.intersects(rightSideWall.getBounds())) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
+         private void formKeyPressed(java.awt.event.KeyEvent evt) {                                
+        //prints KeyCode for the key pressed
+        // System.out.println(evt.getKeyCode());
+
+        //up key pressed
+        if (evt.getKeyCode() == 38) {
+            if (!checkCollision(tataPlane, 0, -10)) {
+                tataPlane.setLocation(tataPlane.getLocation().x, tataPlane.getLocation().y - 10);
+            }
+        }
+        //down key pressed
+        if (evt.getKeyCode() == 40) {
+            if (!checkCollision(tataPlane, 0, 10)) {
+                tataPlane.setLocation(tataPlane.getLocation().x, tataPlane.getLocation().y + 10);
+            }
+        }
+        //left key pressed
+        if (evt.getKeyCode() == 37) {
+            if (!checkCollision(tataPlane, -10, 0)) {
+                tataPlane.setLocation(tataPlane.getLocation().x - 10, tataPlane.getLocation().y);
+            }
+        }
+        //right key pressed
+        if (evt.getKeyCode() == 39) {
+            if (!checkCollision(tataPlane, 10, 0)) {
+                tataPlane.setLocation(tataPlane.getLocation().x + 10, tataPlane.getLocation().y);
+            }
+        }
+    }                       
+        
+    public AlexForm() {
+        initComponents();
+        
+         myInitComponents(tataPlane);
+    }
+
+      public void myInitComponents(javax.swing.JLabel jLabel1) {
+        //Initialize a Buffered Image
+        BufferedImage img = null;
+        //set the Buffered Image to the picture file
+        try {
+            img = ImageIO.read(new File("tataPlane.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //Create a temporary Image and scale it to the size of the label 
+        Image tempImg = img.getScaledInstance(jLabel1.getWidth(), jLabel1.getHeight(),
+                Image.SCALE_SMOOTH);
+        //Create and Image Icon from the new scaled image
+        ImageIcon imageIcon = new ImageIcon(tempImg);
+        //Set the label's icon property to the new icon
+        jLabel1.setIcon(imageIcon);
+
+        
+        //same as above, but in a condensed version
+        try {
+            rightSideWall.setIcon(new ImageIcon((ImageIO.read(new File("hellWallRight.jpg"))).getScaledInstance(rightSideWall.getWidth(), rightSideWall.getHeight(), Image.SCALE_SMOOTH)));
+        } catch (IOException ex) {
+            Logger.getLogger(AlexForm.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -26,21 +107,42 @@ public class AlexForm extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        tataPlane = new javax.swing.JLabel();
+        rightSideWall = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setAutoRequestFocus(false);
         setName("Form"); // NOI18N
         setPreferredSize(new java.awt.Dimension(1080, 720));
         setResizable(false);
         setSize(new java.awt.Dimension(1080, 720));
 
+        org.jdesktop.application.ResourceMap resourceMap = org.jdesktop.application.Application.getInstance(thefaultinourbaldspot.TheFaultInOurBaldSpotApp.class).getContext().getResourceMap(AlexForm.class);
+        tataPlane.setIcon(resourceMap.getIcon("tataPlane.icon")); // NOI18N
+        tataPlane.setText(resourceMap.getString("tataPlane.text")); // NOI18N
+        tataPlane.setName("tataPlane"); // NOI18N
+
+        rightSideWall.setIcon(resourceMap.getIcon("rightSideWall.icon")); // NOI18N
+        rightSideWall.setText(resourceMap.getString("rightSideWall.text")); // NOI18N
+        rightSideWall.setName("rightSideWall"); // NOI18N
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1020, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(480, 480, 480)
+                .addComponent(tataPlane, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 440, Short.MAX_VALUE)
+                .addComponent(rightSideWall, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 720, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(tataPlane, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(39, 39, 39))
+            .addComponent(rightSideWall, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -82,5 +184,7 @@ public class AlexForm extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel rightSideWall;
+    private javax.swing.JLabel tataPlane;
     // End of variables declaration//GEN-END:variables
 }
