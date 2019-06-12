9e0427b5656f88d424a122dba994e3591a15a0a7
ABullOnAKnoll <50926834+ABullOnAKnoll@users.noreply.github.com>
2019-06-12 17:02:15 -0400

    Added the E

--- TheFaultInOurBaldSpot/src/Brayden/character.java
+++ TheFaultInOurBaldSpot/src/Brayden/character.java
@@ -13,12 +13,42 @@ public class character {
 private String iteam;
 private String skin;
 private int speed;
+private Boolean potatoI;
+private Boolean tomatoI;
+private Boolean mushroomI;
+private Boolean potatoS;
+private Boolean tomatoS;
+private Boolean mushroomS;
 
 public character(){
  iteam = "";
  skin = "braydenNude.png";
  speed = 20;
-    
+ potatoI = false;
+ tomatoI = false;
+ mushroomI = false;
+ potatoS = false;
+ tomatoS = false;
+ mushroomS = false; 
+
+    if (potatoI = true) {
+iteam = "potatoBox.png";
+    }
+    if (tomatoI = true) {
+iteam = "tomatoBox.png";
+    }
+    if (mushroomI = true) {
+iteam = "mushroomBox.png";
+    }
+    if (tomatoS = true) {
+iteam = "potatoBox.png";
+    }
+    if (mushroomS = true) {
+iteam = "potatoBox.png";
+    }
+    if (potatoS = true) {
+iteam = "potatoBox.png";
+    }
 }
     
 }
