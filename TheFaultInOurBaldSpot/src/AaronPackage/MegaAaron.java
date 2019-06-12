4f8da9142e2eb4147f71bb019bde42be7f154ae9
epicness676 <50926594+epicness676@users.noreply.github.com>
2019-06-12 13:57:59 -0400

    Added collision on blocks

---
+++ TheFaultInOurBaldSpot/src/AaronPackage/MegaAaron.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AaronPackage;
+
+/**
+ *
+ * @author aaron.bester708
+ */
+public class MegaAaron extends PlayerClass {
+ 
+    public MegaAaron() {
+        health = 10;
+        speed = 3;
+        strength = 3;
+        
+    }
+}
