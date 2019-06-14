18eb9d3956389b5ed0e52faed046c67badf7dead
BillNyeTheCommunistGuy <50926823+BillNyeTheCommunistGuy@users.noreply.github.com>
2019-06-14 17:15:02 -0400

    TryingHighScores

--- TheFaultInOurBaldSpot/src/AlexPackage/scores.java
+++ TheFaultInOurBaldSpot/src/AlexPackage/scores.java
@@ -26,12 +26,11 @@ public class scores extends javax.swing.JFrame {
         initComponents();
          jLabel1.setText("Game High Scores");
          //dummy varible
-        BufferedReader readFile = null;
-        String reading = "";
-        int intReading = 0;
-        //array list to store the data from the file 
-        ArrayList myArrayList= new ArrayList<Integer>();
+        BufferedReader readFile;
 
+        //array list to store the data from the file 
+        ArrayList ScoreArrayList= new ArrayList<Integer>();
+        ArrayList InitialsArrayList= new ArrayList<String>();
         //OptionPane to ask the reader what local file they would like the program to read
         //NO SAFE GAURD FOR THE NULL POINTER EXCEPTION, THIS HAS BEEN APPROVED BY MR.V
        
@@ -39,50 +38,70 @@ public class scores extends javax.swing.JFrame {
         readFile = new BufferedReader(new FileReader("HighScoresAlex.txt"));
                
         //do loop to continue to get data until theres no more lines to read
+    String myLine;
         do {
-            //sets the line of the external file to the dummy varible
-            reading = readFile.readLine();
-            //if statment to not get a "null" line when printing the array 
-            if (reading != null) {
-                //adds line to array
-                try {
-                intReading = Integer.parseInt(reading);
-                }catch (NumberFormatException e){
-                        }
-                myArrayList.add(intReading);
+            //myLine is equal to line being read
+            myLine = readFile.readLine();
+            if (myLine == null) {
+               // if nothing is there to be read, the loop is exited
+                break;
+            } else {
+                //adds the number to its arraylist
+                InitialsArrayList.add(myLine);
+                //reads the next line
+                myLine = readFile.readLine();
+                // adds that line to the psalm arraylist
+                ScoreArrayList.add(myLine);
             }
-        } while (reading != null);
-        //closes text file as thats proper coding 
-        readFile.close();
+        } while (myLine != null);   
+        System.out.println("");
+    
+        
+        
         //prints the text, uses for loop to print each line on its own line
-        for (int i = 0; i < myArrayList.size(); i++) {
-            System.out.println(myArrayList.get(i));
+        for (int i = 0; i < ScoreArrayList.size(); i++) {
+            System.out.println(ScoreArrayList.get(i));
         }
-         selectionSort(myArrayList);
-         System.out.println("These numbers in a sorted list are: " + myArrayList);
-         highScore1.setText(String.valueOf(myArrayList.get(0)));
-         highScore2.setText(String.valueOf(myArrayList.get(1))); 
-         highScore3.setText(String.valueOf(myArrayList.get(2))); 
+         selectionSort(ScoreArrayList, InitialsArrayList);
+         System.out.println("These numbers in a sorted list are: " + ScoreArrayList);
+         highScore1.setText(String.valueOf(ScoreArrayList.get(0)));
+         highScore2.setText(String.valueOf(ScoreArrayList.get(1))); 
+         highScore3.setText(String.valueOf(ScoreArrayList.get(2)));
+         highScore4.setText(String.valueOf(ScoreArrayList.get(3)));
+         highScore5.setText(String.valueOf(ScoreArrayList.get(4)));
+         
+         Initial1.setText(String.valueOf(InitialsArrayList.get(0)));
+         Initial2.setText(String.valueOf(InitialsArrayList.get(1))); 
+         Initial3.setText(String.valueOf(InitialsArrayList.get(2)));
+         Initial4.setText(String.valueOf(InitialsArrayList.get(3)));
+         Initial5.setText(String.valueOf(InitialsArrayList.get(4)));
+//         
+         
     }
 
-    public static void selectionSort(ArrayList myArrayList) {
+    public static void selectionSort(ArrayList <Integer> myArrayList, ArrayList <String> TextArray) {
         int smallest;
          for (int i = 0; i < myArrayList.size() - 1; i++) {
                 smallest = i;
                 //see if there is a smaller number further in the array
                 for (int index = i + 1; index < myArrayList.size(); index++) {
                     //the conditions in this if statement are switched (compared to the previous statement) to make it descending 
-                    if ((Integer) myArrayList.get(smallest) < (Integer) myArrayList.get(index)) {
-                        swap(myArrayList, smallest, index);
+if(5<10){                    
+//if ( myArrayList.get(smallest) <  myArrayList.get(index)) {
+                        swap(myArrayList, TextArray,smallest, index);
                     }
                 }
         }
     }
-    
-    public static void swap(ArrayList array2, int first, int second) {
-        int hold = (Integer) array2.get(first);
+    //TextArray,
+    //ArrayList array3,
+    public static void swap(ArrayList<Integer> array2, ArrayList<String> array3, int first, int second) {
+        Integer hold = (Integer) array2.get(first);
         array2.set(first, array2.get(second));
         array2.set(second, hold);
+        String hold2 =  array3.get(first);
+        array3.set(first, array3.get(second));
+        array3.set(second,hold2);
     }
 
     /**
@@ -98,6 +117,13 @@ public class scores extends javax.swing.JFrame {
         highScore1 = new javax.swing.JLabel();
         highScore2 = new javax.swing.JLabel();
         highScore3 = new javax.swing.JLabel();
+        highScore4 = new javax.swing.JLabel();
+        highScore5 = new javax.swing.JLabel();
+        Initial1 = new javax.swing.JLabel();
+        Initial2 = new javax.swing.JLabel();
+        Initial3 = new javax.swing.JLabel();
+        Initial4 = new javax.swing.JLabel();
+        Initial5 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMinimumSize(new java.awt.Dimension(600, 600));
@@ -116,22 +142,75 @@ public class scores extends javax.swing.JFrame {
         jLabel1.setBounds(10, 11, 580, 80);
 
         highScore1.setFont(resourceMap.getFont("highScore1.font")); // NOI18N
+        highScore1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         highScore1.setText(resourceMap.getString("highScore1.text")); // NOI18N
         highScore1.setName("highScore1"); // NOI18N
         getContentPane().add(highScore1);
-        highScore1.setBounds(10, 109, 80, 20);
+        highScore1.setBounds(300, 100, 100, 40);
 
         highScore2.setFont(resourceMap.getFont("highScore2.font")); // NOI18N
+        highScore2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         highScore2.setText(resourceMap.getString("highScore2.text")); // NOI18N
+        highScore2.setToolTipText(resourceMap.getString("highScore2.toolTipText")); // NOI18N
         highScore2.setName("highScore2"); // NOI18N
         getContentPane().add(highScore2);
-        highScore2.setBounds(10, 147, 80, 20);
+        highScore2.setBounds(300, 200, 100, 40);
 
         highScore3.setFont(resourceMap.getFont("highScore3.font")); // NOI18N
+        highScore3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         highScore3.setText(resourceMap.getString("highScore3.text")); // NOI18N
         highScore3.setName("highScore3"); // NOI18N
         getContentPane().add(highScore3);
-        highScore3.setBounds(10, 185, 80, 20);
+        highScore3.setBounds(300, 300, 100, 40);
+
+        highScore4.setFont(resourceMap.getFont("highScore4.font")); // NOI18N
+        highScore4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        highScore4.setText(resourceMap.getString("highScore4.text")); // NOI18N
+        highScore4.setName("highScore4"); // NOI18N
+        getContentPane().add(highScore4);
+        highScore4.setBounds(300, 400, 100, 40);
+
+        highScore5.setFont(resourceMap.getFont("highScore5.font")); // NOI18N
+        highScore5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        highScore5.setText(resourceMap.getString("highScore5.text")); // NOI18N
+        highScore5.setName("highScore5"); // NOI18N
+        getContentPane().add(highScore5);
+        highScore5.setBounds(300, 500, 100, 40);
+
+        Initial1.setFont(resourceMap.getFont("Initial1.font")); // NOI18N
+        Initial1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        Initial1.setText(resourceMap.getString("Initial1.text")); // NOI18N
+        Initial1.setName("Initial1"); // NOI18N
+        getContentPane().add(Initial1);
+        Initial1.setBounds(200, 100, 100, 40);
+
+        Initial2.setFont(resourceMap.getFont("Initial2.font")); // NOI18N
+        Initial2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        Initial2.setText(resourceMap.getString("Initial2.text")); // NOI18N
+        Initial2.setName("Initial2"); // NOI18N
+        getContentPane().add(Initial2);
+        Initial2.setBounds(200, 200, 100, 40);
+
+        Initial3.setFont(resourceMap.getFont("Initial3.font")); // NOI18N
+        Initial3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        Initial3.setText(resourceMap.getString("Initial3.text")); // NOI18N
+        Initial3.setName("Initial3"); // NOI18N
+        getContentPane().add(Initial3);
+        Initial3.setBounds(200, 300, 100, 40);
+
+        Initial4.setFont(resourceMap.getFont("Initial4.font")); // NOI18N
+        Initial4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        Initial4.setText(resourceMap.getString("Initial4.text")); // NOI18N
+        Initial4.setName("Initial4"); // NOI18N
+        getContentPane().add(Initial4);
+        Initial4.setBounds(200, 400, 100, 40);
+
+        Initial5.setFont(resourceMap.getFont("Initial5.font")); // NOI18N
+        Initial5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        Initial5.setText(resourceMap.getString("Initial5.text")); // NOI18N
+        Initial5.setName("Initial5"); // NOI18N
+        getContentPane().add(Initial5);
+        Initial5.setBounds(200, 500, 100, 40);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -176,9 +255,16 @@ public class scores extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel Initial1;
+    private javax.swing.JLabel Initial2;
+    private javax.swing.JLabel Initial3;
+    private javax.swing.JLabel Initial4;
+    private javax.swing.JLabel Initial5;
     private javax.swing.JLabel highScore1;
     private javax.swing.JLabel highScore2;
     private javax.swing.JLabel highScore3;
+    private javax.swing.JLabel highScore4;
+    private javax.swing.JLabel highScore5;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
