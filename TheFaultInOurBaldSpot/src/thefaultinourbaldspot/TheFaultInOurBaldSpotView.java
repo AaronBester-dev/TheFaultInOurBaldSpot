c7c50108b75ed67efa8309b2b0d4559003a07875
JohnDiemert <50926912+JohnDiemert@users.noreply.github.com>
2019-06-17 14:08:41 -0400

    moved things

--- TheFaultInOurBaldSpot/src/thefaultinourbaldspot/TheFaultInOurBaldSpotView.java
+++ TheFaultInOurBaldSpot/src/thefaultinourbaldspot/TheFaultInOurBaldSpotView.java
@@ -171,7 +171,7 @@ public class TheFaultInOurBaldSpotView extends FrameView {
             }
         });
         mainPanel.add(john);
-        john.setBounds(760, 610, 200, 50);
+        john.setBounds(740, 610, 200, 50);
 
         background.setName("background"); // NOI18N
         background.setPreferredSize(new java.awt.Dimension(1080, 720));
